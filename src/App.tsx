c983fb058262b7f2623311b3fd2193a0d990fce1
Dmitry Tyzhnenko <t.dmitry@gmail.com>
2023-03-21 20:17:35 +0200

    Add GA

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import AppRouter from "./hoc/AppRouter";
 
+import ReactGA from "react-ga4";
+
+const TRACKING_ID = "G-D3Y8ZC86BD";
+ReactGA.initialize(TRACKING_ID);
+
 function App() {
   return (
     <React.StrictMode>
